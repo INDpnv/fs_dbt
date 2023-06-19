3464145d44ee9b761d084ec6abd50ecda41d94df
Pranav <mbpranav@gmail.com>
2023-06-19 16:33:36 -0400

    cleanup

--- fs_dbt/models/semantic/sql/sales.sql
+++ fs_dbt/models/semantic/sql/sales.sql
@@ -11,10 +11,10 @@ with date_dim as (
     from {{ ref('dim_date') }}
 )
 select
-    {{ dbt_utils.star(from=ref('fct_sales'), relation_alias='fs', except=[
+    {{ dbt_utils.star(from=ref('fct_orders'), relation_alias='fs', except=[
         "sales_key", "customer_key", "order_date_key"]) }},
     {{ dbt_utils.star(from=ref('dim_customer'), relation_alias='c', except=["customer_key", "customer_id"]) }},
     date_dim.date_day as order_date
-from {{ ref('fct_sales') }} fs
+from {{ ref('fct_orders') }} fs
 left join {{ ref('dim_customer') }} c on fs.customer_key = c.customer_key
 left join date_dim on fs.order_date_key = date_dim.date_key
